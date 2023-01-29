37bb3d68fac512924a84b50b3cd683f4b515605d
rocmisol <rocmisol@gmail.com>
2023-01-29 20:30:33 +0100

    JavaDoc

--- cuentas/CCuenta.java
+++ cuentas/CCuenta.java
@@ -1,35 +1,65 @@
 package cuentas;
+/**
+ * 
+ * @author Rocío Prieto
+ * @version 01
+ * @since 29/01/2023
+ *
+ */
 public class CCuenta {
 
 
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
+    /**
+     * 
+     */
     public CCuenta()
     {
     }
-
+    /**
+     * 
+     * Método constructor CCuenta
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
     }
-
+    
+    /**
+     * Método estado
+     * @return saldo
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Método ingresar
+     * @param cantidad
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    /**
+     * Método retirar
+     * @param cantidad
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -84,14 +114,14 @@ public class CCuenta {
 	/**
 	 * @return the tipoInterés
 	 */
-	private double getTipoInterés() {
-		return tipoInterés;
+	private double getTipoInteres() {
+		return tipoInteres;
 	}
 
 	/**
 	 * @param tipoInterés the tipoInterés to set
 	 */
-	private void setTipoInterés(double tipoInterés) {
-		this.tipoInterés = tipoInterés;
+	private void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
 	}
 }
